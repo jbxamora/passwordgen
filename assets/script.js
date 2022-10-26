c6e8790fa937af3f5421889e63d0c59600ea5084
jbxamora <jbxamora@icloud.com>
2022-10-25 23:21:24 -0700

    Added generator functions and Dom elements

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,49 @@
-// Assignment code here
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  Symbol: getRandomSymbol
+};
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.ariaChecked;
+  const hasUpper = uppercaseEl.ariaChecked;
+  const hasNumber = numbersEl.ariaChecked;
+  const hasSymbol = symbolsEl.ariaChecked;
+
+  console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+})
+
+// Generator Functions 
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) * 97)
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()_+-=?/.,{}';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomNumber())
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
