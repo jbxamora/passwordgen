256cafe28576d1f4fbf0cf6f954bc4e89ffd0c66
jbxamora <jbxamora@icloud.com>
2022-10-27 22:47:36 -0700

    Went down rabbit hole, will complete only acceptance criteria

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,3 @@
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  Symbol: getRandomSymbol
-};
-
-// Generate Password Event
-
-generateEl.addEventListener('click', () => {
-  const length = +lengthEl.value;
-  const hasLower = lowercaseEl.checked;
-  const hasUpper = uppercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
-
-resultEl.innertext = generatePassword(
-  hasLower,
-  hasUpper, 
-  hasNumber, 
-  hasSymbol, 
-  length
-  );
-})
-
-// Generate Password function
-function generatePassword(lower, upper, number, symbol, length) {
-  // 1. init pw var
-  // 2. filter out unchecked types
-  // 3. loop over length call generator function for each type
-  // 4. add final pw to pw var and return
-}
-
-// Generator Functions 
-// Functions - http://www.net-comber.com/charset.html
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
-}
-
-// Generate a Letter(uppercase)
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-// Generate a Number
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-// Generates a Symbol
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*()_+-=?/.,{}[]';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
 console.log(getRandomNumber())
 
 // Get references to the #generate element
@@ -75,5 +12,132 @@ function writePassword() {
 
 } 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Update after grading. Add checkboxes - Change stylesheet - Revert HTML to match events
+
+
+// const resultEl = document.getElementById('result');
+// const lengthEl = document.getElementById('length');
+// const uppercaseEl = document.getElementById('uppercase');
+// const lowercaseEl = document.getElementById('lowercase');
+// const numbersEl = document.getElementById('numbers');
+// const symbolsEl = document.getElementById('symbols');
+// const generateEl = document.getElementById('generate');
+// const clipboardEl = document.getElementById('clipboard');
+
+// const randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   Symbol: getRandomSymbol
+// };
+
+// // Copy to Clibboard
+// clipboardEl.addEventListener('click', () => {
+//   const textarea = document.createElement('textarea');
+//   const password = resultEl.innerText;
+
+//   if(!password) {
+//     return;
+//   }
+// // execCommand is deprecated but no alternative?..
+//   textarea.value = password;
+//   document.body.appendChild(textarea);
+//   textarea.select();
+//   document.execCommand('copy');
+//   alert('Password Copied To Clipboard!');
+// });
+
+// // Generate Password Event
+
+// generateEl.addEventListener('click', () => {
+//   const length = +lengthEl.value;
+//   const hasLower = lowercaseEl.checked;
+//   const hasUpper = uppercaseEl.checked;
+//   const hasNumber = numbersEl.checked;
+//   const hasSymbol = symbolsEl.checked;
+
+// resultEl.innertext = generatePassword(
+//   hasLower,
+//   hasUpper, 
+//   hasNumber, 
+//   hasSymbol, 
+//   length
+//   );
+// })
+
+// // Generate Password function
+// function generatePassword(lower, upper, number, symbol, length) {
+//   // 1. init pw var
+//   // 2. filter out unchecked types
+//   // 3. loop over length call generator function for each type
+//   // 4. add final pw to pw var and return
+
+//   let generatedPassword = '';
+
+//   const typesCount = lower + upper + number + symbol;
+
+//   console.log('typesCount: ', typesCount);
+
+//   const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+//   (
+//     item => Object.values(item)[0]
+//   );
+
+//   console.log('typesArr: ', typesArr);
+
+//   if(typesCount === 0) {
+//     return '';
+//   }
+
+//   for(let i = 0; i < length; i += typesCount) {
+//     typesArr.forEach(type => {
+//       const funcName = Object.keys(type)[0];
+//       console
+
+//       generatedPassword += randomFunc[funcName]()
+//     })
+//   }
+// const finalPassword = generatedPassword.slice(0, length);
+
+// return finalPassword;
+// }
+
+// // Generator Functions 
+// // Functions - http://www.net-comber.com/charset.html
+
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+// }
+
+// // Generate a Letter(uppercase)
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+// }
+// // Generate a Number
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+// }
+// // Generates a Symbol
+// function getRandomSymbol() {
+//   const symbols = '!@#$%^&*()_+-=?/.,{}[]';
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
+
+
+
